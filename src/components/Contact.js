f884ae6e367c52c80b04b905b9d96d79ba4535ac
aryandesai1 <aryandesai34@gmail.com>
2024-10-30 14:35:37 -0400

    it message for your changes. Lines starting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,46 +20,52 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/contact', formData);
-      setResponseMessage(response.data.message);
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/contact`, formData);
+
+      // Assuming the Flask backend sends a success message
+      setResponseMessage(response.data.message || 'Form submitted successfully!');
     } catch (error) {
       console.error('Error submitting form:', error);
-      setResponseMessage('Something went wrong.');
+
+      // Display a specific message if the backend provides it, otherwise, a default message
+      const errorMessage = error.response?.data?.message || 'Something went wrong while submitting the form.';
+      setResponseMessage(errorMessage);
     }
+
   };
 
   return (
     <section id="contact-us" className="section">
       <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Your Name"
-        value={formData.name}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Your Email"
-        value={formData.email}
-        onChange={handleChange}
-        required
-      />
-      <textarea
-        name="message"
-        placeholder="Your Message"
-        value={formData.message}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit">Send</button>
-      <p>{responseMessage}</p>
-    </form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Your Message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Send</button>
+        <p>{responseMessage}</p>
+      </form>
 
     </section>
-    
+
   );
 };
 
